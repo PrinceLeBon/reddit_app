b1965a8f84c518b32bcd1c792ed869bccc3a0f99
PrinceLeBon <jeffersonhoundjo@gmail.com>
2023-03-07 15:59:31 +0100

    Setup Authorization Code flow

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_web_auth/flutter_web_auth.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -22,6 +26,7 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
+
   final String title;
 
   @override
@@ -29,13 +34,13 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  static const String _clientId = 'cRySjh0bQCJK5E0B0ViNQw';
+  static const String _redirectUri = 'redditapp://oauth2callback';
+  static const String _authEndpoint =
+      'https://www.reddit.com/api/v1/authorize.compact?';
+  static const String _tokenEndpoint =
+      'https://www.reddit.com/api/v1/access_token';
+  String token = "c";
 
   @override
   Widget build(BuildContext context) {
@@ -47,12 +52,8 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              'You have pushed the button this many times: $token',
             ),
           ],
         ),
@@ -63,5 +64,48 @@ class _MyHomePageState extends State<MyHomePage> {
         child: const Icon(Icons.add),
       ),
     );
+  }
+
+  void _incrementCounter() async {
+    String? access = await _getAuthorizationCode();
+    setState(() {
+      _getAccessToken(access);
+    });
+  }
+
+  Future<String?> _getAuthorizationCode() async {
+    const authorizationUrl = '$_authEndpoint'
+        'client_id=$_clientId'
+        '&response_type=code'
+        '&state=RANDOM_STRING'
+        '&redirect_uri=$_redirectUri'
+        '&duration=temporary'
+        '&scope=read';
+    final result = await FlutterWebAuth.authenticate(
+        url: authorizationUrl, callbackUrlScheme: "redditapp");
+    final uri = Uri.parse(result);
+    final queryParams = uri.queryParameters;
+    print(queryParams['code']);
+    return queryParams['code'];
+  }
+
+  Future<String> _getAccessToken(String? authorizationCode) async {
+    print(authorizationCode);
+    final tokenResponse = await http.post(Uri.parse(_tokenEndpoint), headers: {
+      'Authorization': 'Basic ${base64Encode(utf8.encode("$_clientId:"))}'
+    }, body: {
+      'grant_type': 'authorization_code',
+      'code': authorizationCode,
+      'redirect_uri': _redirectUri
+    });
+    if (tokenResponse.statusCode == 200) {
+      final tokenJson = jsonDecode(tokenResponse.body);
+      setState(() {
+        token = tokenJson['access_token'];
+      });
+    } else {
+      throw Exception('Failed to fetch data');
+    }
+    return token;
   }
 }
